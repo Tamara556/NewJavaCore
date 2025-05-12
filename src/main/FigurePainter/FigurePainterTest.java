7885644403a14022c76719afb8ea08ded3779d2c
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-12 20:08:19 +0400

    Figure Painter

---
+++ src/main/FigurePainter/FigurePainterTest.java
@@ -0,0 +1,10 @@
+package main.FigurePainter;
+
+public class FigurePainterTest {
+    public static void main(String[] args) {
+        FigurePainter figurePainter = new FigurePainter();
+        figurePainter.figureOne(4, '@');
+        figurePainter.figureOne(8, '@');
+        figurePainter.figureOne(12);
+    }
+}
